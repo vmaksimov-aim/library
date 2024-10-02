c6f3fae73d95bf07e25df8bea42077e7f4aa6656
Vladyslav Maksymov <v.maksimov@aimprosoft.com>
2024-10-02 17:45:38 +0300

    Added license and setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_library',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='Test library',
+    author='Vladyslav Maksymov',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/my_library',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
