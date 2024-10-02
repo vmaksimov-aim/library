789680ec3cb95d8d4529b563f3bcbc78928a5d72
Vladyslav Maksymov <v.maksimov@aimprosoft.com>
2024-10-02 17:57:06 +0300

    Updated setuptool

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_library',
-    version='0.1.0',
+    name='aim-cognito-security-tornado',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[],
     description='Test library',
     author='Vladyslav Maksymov',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/my_library',
+    url='https://github.com/vmaksimov-aim/library',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
